e4edc5774d6ec32be28c1a8cd871adddb277a61c
Nate Paradis <nathaniel.j.paradis@gmail.com>
2026-06-05 12:27:45 -0400

    add note on scoring method to clarify space character inclusion

--- src/primitives.rs
+++ src/primitives.rs
@@ -24,5 +24,7 @@ pub fn xor_two_buffers(buf1: &[u8], buf2: &[u8]) -> Vec<u8> {
 
 pub fn score(bytes: &[u8]) -> usize {
     let lower = bytes.to_ascii_lowercase();
+    // the space at the end here is because spaces are a good sign we've
+    // found a real sentence
     b"etaoinshrdlu ".iter().map(|c| lower.iter().filter(|&&b| b == *c).count()).sum()
 }
